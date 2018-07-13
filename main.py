5221bfc5e9d4c0c446389e3c5df2025671a39064
Evan Benn <evanbenn@gmail.com>
2018-07-13 23:33:33 +1000

    Make it work for a real wdl
    But it is not spec compliant, try to standardise

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ from lark import Lark
 wdl_parser = Lark(r"""
     ?doc: task* workflow task*
 
-    workflow: "workflow" "{" input? (declaration|call)* output? "}"
-    task: "task" "{" input? declaration* command declaration* output? "}"
+    workflow: "workflow" NAME "{" input? workflow_block* output? "}"
+    workflow_block: declaration | call | scatter
+    task: "task" NAME "{" input? declaration* command declaration* runtime? declaration* output? "}"
 
     input: "input" "{" opt_declaration* "}"
     output: "output" "{" declaration* "}"
 
-    call: "call" NAME [ "{" "input:" variable_mapping* "}" ]
+    call: "call" NAME [ "{" "input" variable_mapping* "}" ]
     variable_mapping: NAME "=" expression
 
+    scatter: "scatter" "(" NAME "in" expression ")" "{" workflow_block* "}"
+
     command: "command" /<<<.*?>>>/s
+    runtime: "runtime" "{" variable_mapping* "}"
     
     opt_declaration: type NAME ["=" expression]?
     declaration: type NAME "=" expression
@@ -39,7 +43,7 @@ wdl_parser = Lark(r"""
               | expression "&&" expression
               | expression "||" expression
               | "{" ( expression ":" expression ( "," expression ":" expression )*)? "}"
-              | "[" expression* "]"
+              | "[" ( expression ( "," expression )*)? "]"
               | literal
               | NAME
 
@@ -64,7 +68,9 @@ wdl_parser = Lark(r"""
     %import common.SIGNED_NUMBER
     %import common.CNAME -> NAME
     %import common.WS
+    COMMENT: /#.*/
     %ignore WS
+    %ignore COMMENT
 
     """, start='doc', ambiguity="explicit")
 
@@ -81,7 +87,12 @@ class WDLTransformer(Transformer):
 
 #print(WDLTransformer().transform(wdl_parser.parse('13.1')))
 
-import tests
-for t in tests.t:
-    print(wdl_parser.parse(t).pretty())
+import sys
+if len(sys.argv) > 1:
+    with open(sys.argv[1]) as f:
+        print(wdl_parser.parse(f.read()).pretty())
+else:
+    import tests
+    for t in tests.t:
+        print(wdl_parser.parse(t).pretty())
 
